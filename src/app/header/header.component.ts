7784e33b5bbd7d2a41e2483546f3b743a7ace25e
EvanCharles16 <evancharles1609@gmail.com>
2020-07-20 14:38:03 +0700

    Adding navigation with event Binding and NgIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   navbarOpen = false;
+  @Output() featureSelected = new EventEmitter<string>();
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 }
